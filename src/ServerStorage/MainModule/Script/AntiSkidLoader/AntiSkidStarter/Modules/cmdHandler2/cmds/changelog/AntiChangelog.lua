eb6eef2263dad78f1eb23ad1782c4f45bb8d29fe
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2025-09-09 22:02:31 +0200

    uhh my bad

--- src/ServerStorage/MainModule/Script/AntiSkidLoader/AntiSkidStarter/Modules/cmdHandler2/cmds/changelog/AntiChangelog.lua
+++ src/ServerStorage/MainModule/Script/AntiSkidLoader/AntiSkidStarter/Modules/cmdHandler2/cmds/changelog/AntiChangelog.lua
@@ -1,3 +1,3 @@
 return [[09.09.2025 (DD:MM:YYYY) V14.4.11 patch
 - Bypass breakasset method that stupid owners of skidded require executors misuse for their own adventage
-- ]]
+- Hello world from my new github repo btw!!!]]
