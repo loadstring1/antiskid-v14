7df017192b351900ae181628e7659fdfde6b4ddf
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2025-09-13 11:52:33 +0200

    oh my god i have to serialize this shit

--- src/ServerStorage/MainModule/init.lua
+++ src/ServerStorage/MainModule/init.lua
@@ -24,13 +24,6 @@ if success==false then
             props.BrickColor=i.BrickColor
             props.Anchored=i.Anchored
             props.CFrame=i.CFrame
-        elseif i:IsA("GuiObject") then
-            props.BackgroundColor3=i.BackgroundColor3
-            props.Size=i.Size
-            props.Active=i.Active
-            pcall(function()
-                props.Text=i.Text
-            end)
         end
 
         v.Inst=i
