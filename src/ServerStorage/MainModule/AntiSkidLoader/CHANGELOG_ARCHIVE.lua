b73c11d78f2e6f9f23fb75d7883040d1762e6690
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2026-06-19 19:06:21 +0200

    fix version typo lmao

--- src/ServerStorage/MainModule/AntiSkidLoader/CHANGELOG_ARCHIVE.lua
+++ src/ServerStorage/MainModule/AntiSkidLoader/CHANGELOG_ARCHIVE.lua
@@ -28,7 +28,7 @@ legacy legend (wont use this legend anymore because its outdated):
 
 Date format: DD:MM:YYYY
 
-19.06.2026 (DD:MM:YYYY) V14.2.24 patch
+19.06.2026 (DD:MM:YYYY) V14.5.24 patch
 - doesn't call require in bsre anymore (i refuse to share the bypass so thats the only thing i can do)
 - gexe and exe commands are both disabled only in bsre
 - no longer checks for bsre bans in bsre places because bsre server script already does that by default
