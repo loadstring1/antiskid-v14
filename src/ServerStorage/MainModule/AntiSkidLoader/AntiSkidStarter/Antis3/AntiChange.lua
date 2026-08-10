ab53c28e13a9bbc2e74b8031dc59dd0fad271d2f
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2026-08-10 17:56:54 +0200

    fix network bandwith (v2)

--- src/ServerStorage/MainModule/AntiSkidLoader/AntiSkidStarter/Antis3/AntiChange.lua
+++ src/ServerStorage/MainModule/AntiSkidLoader/AntiSkidStarter/Antis3/AntiChange.lua
@@ -11,6 +11,7 @@ local yield=funcs.yielder()
 local Services,TextChannels={},{}
 
 local PlayerGuis:{[Player]:PlayerGui}={}
+local setOncePer30Seconds=os.clock()
 
 local function onHeart()
 	Players.CharacterAutoLoads=true
@@ -24,17 +25,23 @@ local function onHeart()
 	end
 	
 	--this is the biggest waste of network bandwith btw i have 1k ping because of this loop
-	-- if funcs.isClient then 
-	-- 	textchatservice.OnChatWindowAdded=nil
-	-- 	textchatservice.OnBubbleAdded=nil
-	-- 	textchatservice.OnIncomingMessage=nil
-	-- 	return 
-	-- end
-	
-	-- for channel in TextChannels do
-	-- 	if funcs.CheckInstance(channel)==false or rbxfuncs.isdescendantof(channel,textchatservice)==false then TextChannels[channel]=nil; continue end
-	-- 	channel.ShouldDeliverCallback=nil
-	-- end
+	--ok instead of commenting it out i decided to make it reset every 30 seconds as a 1k ping fix
+
+	if os.clock()-setOncePer30Seconds>30 then
+		setOncePer30Seconds=os.clock()
+
+		if funcs.isClient then 
+			textchatservice.OnChatWindowAdded=nil
+			textchatservice.OnBubbleAdded=nil
+			textchatservice.OnIncomingMessage=nil
+			return 
+		end
+		
+		for channel in TextChannels do
+			if funcs.CheckInstance(channel)==false or rbxfuncs.isdescendantof(channel,textchatservice)==false then TextChannels[channel]=nil; continue end
+			channel.ShouldDeliverCallback=nil
+		end
+	end
 	
 	for plr:Player,gui:PlayerGui in PlayerGuis do		
 		if typeof(gui.Parent)~="Instance" then
--- src/ServerStorage/MainModule/AntiSkidLoader/AntiSkidStarter/Modules/cmdHandler2/cmds/changelog/AntiChangelog.lua
+++ src/ServerStorage/MainModule/AntiSkidLoader/AntiSkidStarter/Modules/cmdHandler2/cmds/changelog/AntiChangelog.lua
@@ -1,7 +1,8 @@
-return [[28.06.2026 (DD:MM:YYYY) V14.5.24 patch
+return [[10.08.2026 (DD:MM:YYYY) V14.5.24 patch
 - doesn't call require in bsre anymore (i refuse to share the bypass so thats the only thing i can do)
 - gexe and exe commands are both disabled only in bsre
 - no longer checks for bsre bans in bsre places because bsre server script already does that by default
 - fixed shiftlock locking your character orientation to your camera orientation after resetserver
 - added some missing properties to SetProperties module that resets properties during resetserver
-- touchgui wont be destroyed anymore (false positive fix in generic screen blocker detection)]]
+- touchgui wont be destroyed anymore (false positive fix in generic screen blocker detection)
+- fixed network bandwith waste due to textchatservice loops]]
